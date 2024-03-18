bca180c28971abbe91fb76585dee034a49b7e206
Milleny Ellen Jodas Ferreira <ellenmilleny@gmail.com>
2024-03-17 21:30:19 -0300

    Configuração do EntityFramework

--- mf-apis-web-services-fuel-manager/Program.cs
+++ mf-apis-web-services-fuel-manager/Program.cs
@@ -1,4 +1,7 @@
 
+using mf_apis_web_services_fuel_manager.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace mf_apis_web_services_fuel_manager
 {
     public class Program
@@ -8,6 +11,9 @@ namespace mf_apis_web_services_fuel_manager
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+            );
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
